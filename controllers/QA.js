fae08f49b3ae0364c2158ad9533666c16b324019
Abdallah Askar <abdoasker.1426@gmail.com>
2023-06-26 05:39:56 +0300

    convert fetch to axios

--- controllers/QA.js
+++ controllers/QA.js
@@ -38,18 +38,22 @@
 
 // module.exports=queryML;
 //
+const axios = require('axios');
+
 
 async function queryML(data) {
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/deepset/roberta-large-squad2",
-		{
-			headers: { Authorization: "Bearer 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" },
-			method: "POST",
-			body: JSON.stringify(data),
-		}
-	);
-	const result = await response.json();
-	return result;
+  try {
+    const response = await axios.post(
+      "https://api-inference.huggingface.co/models/deepset/roberta-large-squad2",
+      data,
+      {
+        headers: { Authorization: "Bearer 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" },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
-
 module.exports = queryML;
